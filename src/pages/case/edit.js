3f2aca70100d660b820baff142999d9f0dab249e
18735108906 <291315268@qq.com>
2017-12-07 18:38:46 +0800

    wq

---
+++ src/pages/case/edit.js
@@ -0,0 +1,27 @@
+/**
+ * Created by ms on 2017/12/6.
+ */
+import React from "react"
+import AdminSider from '../../components/sider.jsx';
+import {Table} from 'antd';
+class CaseEdit extends React.Component{
+    state = {
+        data:[]
+    }
+    render(){
+        let columns = [
+            {title:'xuhao',dataIndex:'id',key:'id'},
+            {title:'name',dataIndex:'name',key:'name'},
+            {title:'type',dataIndex:'type',key:'type'},
+        ]
+        return (
+            <AdminSider keys={'team_add'}>
+                <Table columns={columns} dataSource={this.state.data}/>
+            </AdminSider>
+        )
+    }
+    componentDidMount(){
+        fetch('/api/test').then(r=>r.json()).then(d=>this.setState({data:d}))
+    }
+}
+export default CaseEdit;
---
+++ src/pages/team/edit.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminSider from '../../components/sider.jsx';
+
+
+
+
+class TeamEdit extends React.Component{
+
+}
+export default TeamEdit;
